a3dc1d147743f30afc50178cd1741b0ba595dc10
Md Wasim Akram <46383080+MdWasimAkram@users.noreply.github.com>
2022-10-05 13:05:08 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,37 @@
-import React from 'react'
+import React from 'react';
+import { useState } from 'react'
 
 import '../styles/App.css';
 
 
 const searchArray = ["Newton", "School", "Newton School", "React", "Preact", "Node", "Mongoose", "Angular", "Vue", "Apple", "Microsoft", "Netflix", "Meta"]
 const App = () => {
-
+  const [search, setSearch]= useState("");
   
   return (
+    <>
     <div id="main">
+    <input id="search-input" type="text" placeholder="Search..."
+    onKeyUp={(e)=>{setSearch(e.target.value)
+    }}/>
+    <br/>
       
     </div>
+     {
+      searchArray.filter((val)=> {
+        if(search==""){
+        return;
+        }else if(val.toLowerCase().includes(search))
+        return val
+      }).map((val)=>{
+        return(
+          <ul>
+            <li>{val}</li>
+          </ul>
+        )
+      })
+    }
+</>
   )
 }
 
